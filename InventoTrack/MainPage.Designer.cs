6cf446afca8a95920f09f065875502974388814d
Habib Fabian Fahlesi <habibfabianfahlesi2004@mail.ugm.ac.id>
2023-11-07 14:56:44 +0700

    fix mainpage

--- InventoTrack/MainPage.Designer.cs
+++ InventoTrack/MainPage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InventoTrack
 {
-    partial class Form1
+    partial class MainPage
     {
         /// <summary>
         /// Required designer variable.
@@ -33,17 +33,17 @@
             this.saveButton_Click = new System.Windows.Forms.Button();
             this.deleteButton_Click = new System.Windows.Forms.Button();
             this.updateButton_Click = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.nameTextBox = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.Label();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.priceNUD = new System.Windows.Forms.NumericUpDown();
+            this.quantityNUD = new System.Windows.Forms.NumericUpDown();
             this.categoryBox = new System.Windows.Forms.Label();
             this.priceNumericUpdown = new System.Windows.Forms.Label();
             this.quantityNumericUpDown = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantityNUD)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -61,9 +61,9 @@
             // newButton_Click
             // 
             this.newButton_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newButton_Click.Location = new System.Drawing.Point(18, 158);
+            this.newButton_Click.Location = new System.Drawing.Point(19, 158);
             this.newButton_Click.Name = "newButton_Click";
-            this.newButton_Click.Size = new System.Drawing.Size(322, 40);
+            this.newButton_Click.Size = new System.Drawing.Size(300, 40);
             this.newButton_Click.TabIndex = 1;
             this.newButton_Click.Text = "NEW";
             this.newButton_Click.UseVisualStyleBackColor = true;
@@ -72,9 +72,9 @@
             // saveButton_Click
             // 
             this.saveButton_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.saveButton_Click.Location = new System.Drawing.Point(361, 158);
+            this.saveButton_Click.Location = new System.Drawing.Point(366, 158);
             this.saveButton_Click.Name = "saveButton_Click";
-            this.saveButton_Click.Size = new System.Drawing.Size(322, 40);
+            this.saveButton_Click.Size = new System.Drawing.Size(300, 40);
             this.saveButton_Click.TabIndex = 2;
             this.saveButton_Click.Text = "SAVE";
             this.saveButton_Click.UseVisualStyleBackColor = true;
@@ -85,7 +85,7 @@
             this.deleteButton_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.deleteButton_Click.Location = new System.Drawing.Point(709, 158);
             this.deleteButton_Click.Name = "deleteButton_Click";
-            this.deleteButton_Click.Size = new System.Drawing.Size(322, 40);
+            this.deleteButton_Click.Size = new System.Drawing.Size(300, 40);
             this.deleteButton_Click.TabIndex = 3;
             this.deleteButton_Click.Text = "DELETE";
             this.deleteButton_Click.UseVisualStyleBackColor = true;
@@ -94,79 +94,80 @@
             // updateButton_Click
             // 
             this.updateButton_Click.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.updateButton_Click.Location = new System.Drawing.Point(1057, 158);
+            this.updateButton_Click.Location = new System.Drawing.Point(1052, 158);
             this.updateButton_Click.Name = "updateButton_Click";
-            this.updateButton_Click.Size = new System.Drawing.Size(322, 40);
+            this.updateButton_Click.Size = new System.Drawing.Size(300, 40);
             this.updateButton_Click.TabIndex = 4;
             this.updateButton_Click.Text = "UPDATE";
             this.updateButton_Click.UseVisualStyleBackColor = true;
             this.updateButton_Click.Click += new System.EventHandler(this.button4_Click);
             // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(19, 51);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(518, 26);
-            this.textBox5.TabIndex = 9;
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
-            // 
             // nameTextBox
             // 
-            this.nameTextBox.AutoSize = true;
-            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameTextBox.Location = new System.Drawing.Point(13, 25);
+            this.nameTextBox.Location = new System.Drawing.Point(19, 51);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(68, 25);
-            this.nameTextBox.TabIndex = 10;
-            this.nameTextBox.Text = "Name";
-            this.nameTextBox.Click += new System.EventHandler(this.label1_Click);
+            this.nameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.nameTextBox.Size = new System.Drawing.Size(518, 26);
+            this.nameTextBox.TabIndex = 9;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
-            // comboBox1
+            // TextBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.TextBox1.AutoSize = true;
+            this.TextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox1.Location = new System.Drawing.Point(13, 25);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(68, 25);
+            this.TextBox1.TabIndex = 10;
+            this.TextBox1.Text = "Name";
+            this.TextBox1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] {
             "Makanan",
             "Deterjen",
             "Susu",
             "Rokok"});
-            this.comboBox1.Location = new System.Drawing.Point(19, 113);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(517, 26);
-            this.comboBox1.TabIndex = 17;
+            this.categoryComboBox.Location = new System.Drawing.Point(19, 113);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(517, 26);
+            this.categoryComboBox.TabIndex = 17;
             // 
-            // numericUpDown1
+            // priceNUD
             // 
-            this.numericUpDown1.Increment = new decimal(new int[] {
+            this.priceNUD.Increment = new decimal(new int[] {
             500,
             0,
             0,
             0});
-            this.numericUpDown1.Location = new System.Drawing.Point(709, 52);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
+            this.priceNUD.Location = new System.Drawing.Point(709, 52);
+            this.priceNUD.Maximum = new decimal(new int[] {
             -1530494976,
             232830,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(519, 26);
-            this.numericUpDown1.TabIndex = 18;
-            this.numericUpDown1.Tag = "Rp";
-            this.numericUpDown1.ThousandsSeparator = true;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.priceNUD.Name = "priceNUD";
+            this.priceNUD.Size = new System.Drawing.Size(519, 26);
+            this.priceNUD.TabIndex = 18;
+            this.priceNUD.Tag = "Rp";
+            this.priceNUD.ThousandsSeparator = true;
+            this.priceNUD.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
-            // numericUpDown2
+            // quantityNUD
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(709, 115);
-            this.numericUpDown2.Maximum = new decimal(new int[] {
+            this.quantityNUD.Location = new System.Drawing.Point(709, 115);
+            this.quantityNUD.Maximum = new decimal(new int[] {
             -1530494976,
             232830,
             0,
             0});
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(519, 26);
-            this.numericUpDown2.TabIndex = 19;
-            this.numericUpDown2.Tag = "";
-            this.numericUpDown2.ThousandsSeparator = true;
+            this.quantityNUD.Name = "quantityNUD";
+            this.quantityNUD.Size = new System.Drawing.Size(519, 26);
+            this.quantityNUD.TabIndex = 19;
+            this.quantityNUD.Tag = "";
+            this.quantityNUD.ThousandsSeparator = true;
             // 
             // categoryBox
             // 
@@ -199,7 +200,7 @@
             this.quantityNumericUpDown.TabIndex = 22;
             this.quantityNumericUpDown.Text = "Quantity";
             // 
-            // Form1
+            // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -207,23 +208,23 @@
             this.Controls.Add(this.quantityNumericUpDown);
             this.Controls.Add(this.priceNumericUpdown);
             this.Controls.Add(this.categoryBox);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.quantityNUD);
+            this.Controls.Add(this.priceNUD);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.TextBox1);
             this.Controls.Add(this.nameTextBox);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.updateButton_Click);
             this.Controls.Add(this.deleteButton_Click);
             this.Controls.Add(this.saveButton_Click);
             this.Controls.Add(this.newButton_Click);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Courier New", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Name = "Form1";
+            this.Name = "MainPage";
             this.Text = "InventoTrack";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantityNUD)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -236,11 +237,11 @@
         private System.Windows.Forms.Button saveButton_Click;
         private System.Windows.Forms.Button deleteButton_Click;
         private System.Windows.Forms.Button updateButton_Click;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label nameTextBox;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label TextBox1;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.NumericUpDown priceNUD;
+        private System.Windows.Forms.NumericUpDown quantityNUD;
         private System.Windows.Forms.Label categoryBox;
         private System.Windows.Forms.Label priceNumericUpdown;
         private System.Windows.Forms.Label quantityNumericUpDown;
---
+++ InventoTrack/MainPage.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoTrack
+{
+    public partial class MainPage : Form
+    {
+        DataTable inventory = new DataTable();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inventory.Columns.Add("Name");
+            inventory.Columns.Add("Category");
+            inventory.Columns.Add("Price");
+            inventory.Columns.Add("Quantity");
+
+            dataGridView1.DataSource = inventory;
+
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+               
+                nameTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+                priceNUD.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[3].ToString();
+                quantityNUD.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[4].ToString();
+                String itemToLookFor = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[2].ToString();
+                categoryComboBox.SelectedIndex = categoryComboBox.Items.IndexOf(itemToLookFor);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("There has been an error: " + err);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {       
+            nameTextBox.Text = "";
+            categoryComboBox.SelectedIndex = -1;
+            priceNUD.Text = "0";
+            quantityNUD.Text = "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String name = nameTextBox.Text;
+            String category = categoryComboBox.Text;
+            String price = priceNUD.Text;    
+            String quantity = quantityNUD.Text;
+
+            
+
+            if (nameTextBox.Text== "")
+            {
+                MessageBox.Show("Please enter a name for the item");
+            }
+            else if (categoryComboBox.Text == "")
+            {
+                MessageBox.Show("Please enter a category for the item");
+            }
+            else if (priceNUD.Text == "")
+            {
+                MessageBox.Show("Please enter a price for the item");
+            }
+            else if (quantityNUD.Text == "")
+            {
+                MessageBox.Show("Please enter a quantity for the item");
+            }
+            else
+            {
+                inventory.Rows.Add(name, category, price, quantity);
+                button1_Click(sender, e);
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inventory.Rows[dataGridView1.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: " + err);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
